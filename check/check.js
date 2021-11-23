f3de388d84d08590e64e9c3b0a8cd688638b0cf4
Jordan Bean <jordanbean@microsoft.com>
2021-11-23 13:00:07 -0600

    coach

--- check/check.js
+++ check/check.js
@@ -1,14 +1,24 @@
 const path = require('path')
 const fs = require('fs')
 
-let checkIfContainsReadmeInRootDirectory = (inputPath) => {
-  files = fs.readdirSync(inputPath, { withFileTypes: true });
+module.exports = {
+  checkIfContainsReadmeInRootDirectory: (inputPath) => {
+    files = fs.readdirSync(inputPath, { withFileTypes: true });
 
-  return files.some((file) => {
-    if(file.isFile && file.name == 'README.md') {
-      return true;
-    }
-  });
-}
+    return files.some((file) => {
+      if(file.isFile && file.name == 'README.md') {
+        return true;
+      }
+    });
+  },
 
-module.exports = checkIfContainsReadmeInRootDirectory;
+  checkIfContainsCoachInRootDirectory:  (inputPath) => {
+    files = fs.readdirSync(inputPath, { withFileTypes: true });
+
+    return files.some((file) => {
+      if(file.isDirectory && file.name == 'Coach') {
+        return true;
+      }
+    });
+  }
+};
--- check/check.test.js
+++ check/check.test.js
@@ -1,18 +1,26 @@
 const path = require('path');
-const checkIfContainsReadmeInRootDirectory = require('./check');
+const { checkIfContainsReadmeInRootDirectory, checkIfContainsCoachInRootDirectory } = require('./check');
 
 const testDir = path.join(__dirname, '../tst')
 
+//README tests
 test('Root directory does contain a README.md', () => {
-
   const inputPath = path.join(testDir, 'valid');
-
   expect(checkIfContainsReadmeInRootDirectory(inputPath)).toBeTruthy();
 });
 
-test('Root directory does not contain a README.md', () => {
-
+test('Root directory must contain a README.md', () => {
   const inputPath = path.join(testDir, 'invalid', 'doesNotContainReadme');
-
   expect(checkIfContainsReadmeInRootDirectory(inputPath)).toBeFalsy();
+});
+
+//Coach tests
+test('Root directory does contain a Coach directory', () => {
+  const inputPath = path.join(testDir, 'valid');
+  expect(checkIfContainsCoachInRootDirectory(inputPath)).toBeTruthy();
+});
+
+test('Root directory must contain a Coach directory', () => {
+  const inputPath = path.join(testDir, 'invalid', 'doesNotContainCoachDirectory');
+  expect(checkIfContainsCoachInRootDirectory(inputPath)).toBeFalsy();
 });
